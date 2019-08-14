38220832036378c22c8743513d5e73e50b417356
Chiracolivier <chiracolivier28@gmail.com>
2019-08-14 20:07:42 +0200

    Arrange some functions in main class

--- src/com/company/Guerrier.java
+++ src/com/company/Guerrier.java
@@ -14,6 +14,15 @@ public class Guerrier extends Joueur1 {
         super.Caracteristics();
     }
 
+    public void attaque(){
+        Scanner sc3= new Scanner(System.in);
+        System.out.println(" Veuillez choisir votre action : ( 1: Basique, 2: Spéciale)");
+        int attac = sc3.nextInt();
+        if (attac==1){
+            System.out.println("Joueur Utilise Coup d'Epée et inflige "+ this.force+" dommages");
 
+        }else if (attac==2){
+            System.out.println("Joueur  Utilise Coup de Rage et inflige  "+ (this.force)*2+ " dommages");
+        }
 }
 }
